55f152e1fe80e404587de1fc20b02ff652d2fa16
pame1501 <patoapantapi@uide.edu.ec>
2026-03-26 23:53:51 +0000

    Verify_partner's_binary_signature

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -19,3 +19,4 @@ gpg --edit-key jolagunapu@uide.edu.ec #Assign a trust level to your partner's ke
 gpg --sign-key jolagunapu@uide.edu.ec #We signed the document to confirm our decision
 gpg --verify MICOMPA_doc_no_cifrado_firmado.txt #Verify the digital signature of the signed document your colleague sent. GPG already recognizes your colleague's key as trusted, so the verification comes back clean, with no warnings.
 gpg --output doc_no_cifrado_firmado_binario.txt --sign doc_no_cifrado.txt #Sign the document in binary format using the signature in the file
+gpg --verify JOSS_doc_no_cifrado_firmado_binario.txt #Verify partner's binary signed document
