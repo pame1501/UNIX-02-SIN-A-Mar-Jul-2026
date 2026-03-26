57b613bb00a902dd81b4c75d5af664f382c4fd49
pame1501 <patoapantapi@uide.edu.ec>
2026-03-26 12:43:17 +0000

    exportsecretkeys

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -3,4 +3,5 @@ which gpg #muestra ubicación del binario de gpg
 gpg --version #muestra la versión y verifica que todo esté instalado
 gpg --full-generate-key # genero la llave pública y privadausamos RSA por ser comptible y universal 4096 para dar complejidad
 gpg --armor --export # exporta la llave pública
-gpg --list-secret-keys --keyid-format=long # sirve para exportar la llave privada sec
+gpg --list-secret-keys --keyid-format=long # sirve para listar la llave privada sec
+gpg --armor --export-secret-keys 59F9C4BDC3B7F215 # exporta la llave privada usando el hash después de / en sec
