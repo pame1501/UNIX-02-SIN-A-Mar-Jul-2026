d7b226ee9d2a4ac78a4aa024fc20d8d11d48a866
pame1501 <patoapantapi@uide.edu.ec>
2026-03-27 16:44:14 +0000

    Save /proc exploration commands

--- fylesystem_script.sh
+++ fylesystem_script.sh
@@ -1,3 +1,5 @@
 cd /dev #change directory to /dev directory
 ls #list files and directories on the /dev directory
 history # carga lista los comandos realizados
+cd /proc # Real-time system and process information, directly from the terminal.
+cat cpuinfo #Display detailed information about the processor (CPU).
