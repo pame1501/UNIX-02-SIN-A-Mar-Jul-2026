528d6ec61a9ffcfa6e1b61085618ca1d0a721210
pame1501 <patoapantapi@uide.edu.ec>
2026-03-27 17:23:05 +0000

    document pwd command

--- fylesystem_script.sh
+++ fylesystem_script.sh
@@ -6,5 +6,8 @@ cat cpuinfo #Display detailed information about the processor (CPU).
 #en /media se encuentra las particiones de montaje
 bin/ #encontramos archivos bonarios
 var #datos variables como archivos de BDD
-gcc saludo.c -o saludobin # crea un archivo en lenguaje c y luego este se transforma a lenguaje binario
+gcc saludo.c -o saludobin # crea un archivo en lenguaje c y luego este se transforma a lenguaje binario, el -o es output para darle nombre al archivo
 sudo mv saludobin /bin/ # mueve el archivo a /bin para poder ejecutarlo desde cualquier parte de la terminal
+# el inodo almacdena metadatos, el nombre del archivo NO se guarda en el inodo
+#pwd es print working directory
+ls -F # es para listar archivos
