f326390171cd2b2093bee0429c9869ce30131c0b
pame1501 <patoapantapi@uide.edu.ec>
2026-04-02 12:51:55 +0000

    software_install_with_sudo

--- fs_exploration.sh
+++ fs_exploration.sh
@@ -1,2 +1,14 @@
 cd /# Go to the change directory We navigate to the root directory
 ls -F #is used to list the files in the current directory
+Vmlinuz #refers to the Linux kernel
+# initrd es el encargado de iniciar linux
+# FHS is the Linux root directory structure
+#Arranque de linux se divide en 3 fases:
+# 1) carga del firmware
+# -Power on self test
+# -Busqueda del dispositivo de arranque
+# -Busca la BIOS, MBR, UEFI
+sudo apt update # Download an updated list of all available packages and their versions.
+sudo apt upgrade # Download and install the latest versions of the programs you already have on your system.
+sudo apt install parted # Download the files from the Ubuntu/Debian repositories and place them in the system folders
+
