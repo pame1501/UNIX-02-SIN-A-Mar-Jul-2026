8d423146f8f3beed92ac8968571eadf25d17cdda
pame1501 <patoapantapi@uide.edu.ec>
2026-04-09 13:07:37 +0000

    explanation of ls -lai

--- script_boot.sh
+++ script_boot.sh
@@ -15,3 +15,8 @@ man ls # shows me the command options
 ls --all # shows me all we have the directory, the hard links (the . and .. directories) that are present in every directory
 . directory # “.” refers to the current directory
 .. # is a relative link that points to the parent directory of the directory you are currently in.
+ls -lai # displays the inode, the user's owner d(directory) permissions (read, write, execute), the user's group permissions, and permissions for others
+total 8 # displays the number of blocks
+1572971 drwxrwxrwx+ 2 codespace codespace 4096 Apr  9 12:19 . # el número(2) me indica la cantidad de archivos duros apuntando a su inodo
+1572874 drwxrwxrwx+ 5 codespace root      4096 Apr  9 12:19 ..
+#                     user      +   file size + date of creation
