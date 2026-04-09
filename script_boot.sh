8f376766f04d6dd86a8e7531db367d718d051948
pame1501 <patoapantapi@uide.edu.ec>
2026-04-09 12:39:59 +0000

    Environment_var".env"

--- script_boot.sh
+++ script_boot.sh
@@ -5,3 +5,8 @@ mkdir #mkdir is used to create a directory
 #If I use `cd` + `Enter`, it takes me to the home directory no matter which directory I'm in
 #We can also move to the home directory using `cd ~`
 #Finally, we can navigate to a directory using `cd /directory_name`
+#Environment variables are those that can be used anywhere in the environment
+#We use `echo $directory_name` to navigate using environment variables
+echo $HOME #Moves to the HOME directory
+echo $BASH #We use this to move to the bin directory, where the binary executables are located
+echo $PATH #Shows us all the possible directories we can navigate to
