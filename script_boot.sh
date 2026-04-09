c33266c70a2217d35ada1b7b794a3cc2b520ca1a
pame1501 <patoapantapi@uide.edu.ec>
2026-04-09 12:25:09 +0000

    aboslute_relative_path

---
+++ script_boot.sh
@@ -0,0 +1,4 @@
+#The absolute path starts at the root /
+mkdir #mkdir is used to create a directory
+# relative path.- We can navigate to the project folder using cd /directory_name
+#absolute path.- We use it to navigate directly to the root directory
