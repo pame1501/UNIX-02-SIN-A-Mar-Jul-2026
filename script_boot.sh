bc2790e193275c7a33a07941ee14ce5e8eae8020
pame1501 <patoapantapi@uide.edu.ec>
2026-04-09 12:31:56 +0000

    home_directory

--- script_boot.sh
+++ script_boot.sh
@@ -2,3 +2,6 @@
 mkdir #mkdir is used to create a directory
 # relative path.- We can navigate to the project folder using cd /directory_name
 #absolute path.- We use it to navigate directly to the root directory
+#If I use `cd` + `Enter`, it takes me to the home directory no matter which directory I'm in
+#We can also move to the home directory using `cd ~`
+#Finally, we can navigate to a directory using `cd /directory_name`
