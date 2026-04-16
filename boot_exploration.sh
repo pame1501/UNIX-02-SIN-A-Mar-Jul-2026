aa340434f843ee2b7b75c59da69bbeac19ab30bc
pame1501 <patoapantapi@uide.edu.ec>
2026-04-16 12:46:34 +0000

    Joint commands ls

---
+++ boot_exploration.sh
@@ -0,0 +1,4 @@
+ls -a / ls --all # Lists everything, including hidden files
+ls -l # Lists the directories and files I have
+ls -h # Only shows the files in one folder
+ls -l -a -h / ls -lah # Shows the list, the human-readable format, and hidden files; all hidden options
