ee53c9ad06fc20447ab3493b1770e5774d744cab
pame1501 <patoapantapi@uide.edu.ec>
2026-04-16 12:53:55 +0000

    use mkdir and rmdir commands

--- boot_exploration.sh
+++ boot_exploration.sh
@@ -2,3 +2,5 @@ ls -a / ls --all # Lists everything, including hidden files
 ls -l # Lists the directories and files I have
 ls -h # Only shows the files in one folder
 ls -l -a -h / ls -lah # Shows the list, the human-readable format, and hidden files; all hidden options
+mkdir -- -rf # Used to create a directory named -rf
+rmdir -- -rf # Used to remove a directory
