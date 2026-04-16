8644f9e06efd60a0371ac7a9464340f4af6d8f6f
pame1501 <patoapantapi@uide.edu.ec>
2026-04-16 13:01:17 +0000

    help or man commands

--- boot_exploration.sh
+++ boot_exploration.sh
@@ -4,3 +4,6 @@ ls -h # Only shows the files in one folder
 ls -l -a -h / ls -lah # Shows the list, the human-readable format, and hidden files; all hidden options
 mkdir -- -rf # Used to create a directory named -rf
 rmdir -- -rf # Used to remove a directory
+ls --help # Shorter or summary help
+man ls # Displays the full manual
+n to navigate forward and N to navigate backward
