733164ed5b286312cd52a0c95358ec56cf05c554
pame1501 <patoapantapi@uide.edu.ec>
2026-04-23 13:09:35 +0000

    Exercises on file management, permissions (chmod), and using privileges (sudo)

--- boot_exploration.sh
+++ boot_exploration.sh
@@ -26,9 +26,21 @@ sudo chmod +x init # Contains the first process to be executed in the OS
 sudo echo “Hello” > /etc/protected_file # Sudo doesn't work because it only acts on the command immediately following it
 echo “Hello” | sudo tee /etc/protected_file > /dev/null # echo goes to standard output; using | tee gives me the standard output, which in this case is the terminal, and I redirect it to /dev/null
 echo "Hola" | sudo tee /etc/archivo_protegido # displays the file's contents directly in the terminal and creates the file
-sudo sh-c 'echo "chao" >> /etc/archivo_prolegido #Add the line “chao” to the end of a system file securely and with the correct permissions.
+sudo sh-c 'echo "chao" >> /etc/archivo_prolegido # Add the line “chao” to the end of a system file securely and with the correct permissions.
 sudo -i # move to superuser or use sudo su or sudo su -
 su - # move to root but use a password
 rm -rf / # is used to delete all directories
 echo “$HOME” # Expands the variable, displaying its contents
 echo ‘$HOME’ # Prints the text, treating it as a variable
+echo ‘#!/bin/sh’ > hello.sh # This means that whatever you enter will be interpreted by the sh shell
+echo 'echo “Hello from my first script” >> hello.sh # Now you have a text file that contains: echo “Hello from my first script”.
+cat hello.sh # You will see the line you wrote in the previous step on your terminal. This is how you confirm that the file was created correctly.
+/hello.sh # Permission denied. This happens because, for security reasons, new files do not have permission to “run” as programs.
+ls -l Hola.sh # Displays the contents of Hola.sh along with its permissions
+chmod +x Hola.sh # Grants execute permissions
+ls -l Hola.sh # Checks the assigned execute permissions
+./Hola.sh # Moves to the .sh folder
+ls /etc # Lists the files in /etc
+touch /etc/test.txt # Creates a new file that displays “permission denied” because it lacks execute permission
+mkdir ~/my_folder # Creates a folder named my_folder inside your home directory.
+apt install cowsay # Does not allow me to install cowsay
