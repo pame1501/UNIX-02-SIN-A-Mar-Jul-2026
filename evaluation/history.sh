67e923f8addef3ed7a2a7d068115aabe1e0e8cd4
pame1501 <patoapantapi@uide.edu.ec>
2026-04-24 16:56:50 +0000

    Fix problema 6: sticky bit en /tmp/kepler_zone

--- evaluation/history.sh
+++ evaluation/history.sh
@@ -25,12 +25,4 @@
    31  history
    32  ls registros/
    33  history
-   32  ls registros/
-   33  history
-   34  chmod 600 kepler/registros/sensores.log
-   35  chmod 600 registros/sensores.log
-   36  ls -l
-   37  ls -l registros/sensores.log 
-   38  git add .
-   39  git commit -m "Fix problema 3: permisos numericos 600 en sensores.log"
-   40  history
+   
