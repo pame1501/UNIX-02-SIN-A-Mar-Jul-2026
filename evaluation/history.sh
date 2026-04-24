e16c9966c90659247ee2fac63ef04e9cf1d08ec2
pame1501 <patoapantapi@uide.edu.ec>
2026-04-24 16:35:18 +0000

    history 1 evaluation

---
+++ evaluation/history.sh
@@ -0,0 +1,9 @@
+17  mkdir registros
+   18  mv kepler/docs kepler/registros
+   19  mv docs registros
+   20  ls -F
+   21  mkdir plantillas
+   22  ls -F
+   23  git add .
+   24  git commit -m "Fix problema 1: estructura de directorios corregida"
+   25  history
