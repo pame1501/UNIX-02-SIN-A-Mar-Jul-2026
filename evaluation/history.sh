f00cfcdf1e81119e6b7ce44cedb53f427a23571e
pame1501 <patoapantapi@uide.edu.ec>
2026-04-24 16:48:03 +0000

    history

--- evaluation/history.sh
+++ evaluation/history.sh
@@ -7,3 +7,30 @@
    23  git add .
    24  git commit -m "Fix problema 1: estructura de directorios corregida"
    25  history
+   23  git add .
+   24  git commit -m "Fix problema 1: estructura de directorios corregida"
+   25  history
+   26  mv sensores.log registros/
+   27  mv ajustes.old ajustes.conf
+   28  ls -F
+   29  git add .
+   30  git commit -m "Fix problema 2: archivos movidos y renombrados"
+   31  history
+    25  history
+   26  mv sensores.log registros/
+   27  mv ajustes.old ajustes.conf
+   28  ls -F
+   29  git add .
+   30  git commit -m "Fix problema 2: archivos movidos y renombrados"
+   31  history
+   32  ls registros/
+   33  history
+   32  ls registros/
+   33  history
+   34  chmod 600 kepler/registros/sensores.log
+   35  chmod 600 registros/sensores.log
+   36  ls -l
+   37  ls -l registros/sensores.log 
+   38  git add .
+   39  git commit -m "Fix problema 3: permisos numericos 600 en sensores.log"
+   40  history
