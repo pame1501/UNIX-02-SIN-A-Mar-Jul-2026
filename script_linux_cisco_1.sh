7f3bc42c241cd407e28f9c2aac85b59ff77d123c
pame1501 <patoapantapi@uide.edu.ec>
2026-05-03 04:25:40 +0000

    docs: explain the `cd` command for navigation

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -2,3 +2,4 @@ ls #The ls command displays a list of information about the files
 aptitude moo #is a package management tool available in some versions of Linux. 
 ls -l #  The -l option is added to the ls command to produce a “long” output, providing more information about each listed file and allowing you to view technical details that a simple list does not show.
 pwd # We use this command to get information about the current working directory
+cd # We can use `change directory` to switch directories and navigate the file system structure.
