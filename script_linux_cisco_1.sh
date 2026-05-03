1e0b15d6f12d4eca5e7a54faa265e867cc622ce7
pame1501 <patoapantapi@uide.edu.ec>
2026-05-03 04:13:02 +0000

    docs: agregar explicación del comando ls -l

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -1,2 +1,3 @@
 ls #The ls command displays a list of information about the files
 aptitude moo #is a package management tool available in some versions of Linux. 
+ls -l #  The -l option is added to the ls command to produce a “long” output, providing more information about each listed file and allowing you to view technical details that a simple list does not show.
