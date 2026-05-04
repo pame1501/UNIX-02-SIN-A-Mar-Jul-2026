dba0aaafc5f0d283ed0a0b5f1dc0e70c43b2a163
pame1501 <patoapantapi@uide.edu.ec>
2026-05-04 02:58:52 +0000

    docs: description commands to copy file

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -40,3 +40,20 @@ cat # The `cat` command, which stands for “concatenate,” is often used to qu
 #These commands have the same structure as `cat`
 # The -n option with the head and tail commands can be used to specify the number of lines to display.
 # head -n number_of_lines file
+Copy files
+# We use this command to copy files: cp [OPTIONS] ORIGEN DESTINATION
+# The cp command is similar to the mv command because both require a source and a destination
+# Example: cp /etc/passwd . -> In this case . is the destination
+#To copy a file, you must have execute permission to access the directory where the file is located and read permission for the file being copied.
+dd command # The dd command is used to copy files or entire partitions at the bit level.
+#Structure: dd [OPTIONS] OPERATING
+# It can be used for:
+#Clone or delete
+#Copy raw data to removable devices such as USB drives or CD-ROMs.
+#Back up and restore the MBR
+#Create a file of a specific size filled with binary zeros as virtual memory
+# The dd command uses special arguments such as
+#if: The input file to be read. 
+#of: The output file to be written to.
+#bs: The block size to be used. By default, the value is expressed in bytes.
+#count: The number of blocks to be read from the input file. It is not necessary to specify the block size here.
