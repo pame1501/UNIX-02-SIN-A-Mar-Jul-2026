1f80e5e4c2edfb0c2b740d9c7d9985c6d673dee2
pame1501 <patoapantapi@uide.edu.ec>
2026-05-08 16:21:30 +0000

    delete files

--- saludo.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h> /*se incluye una entrada y salida estandar*/
-int main(){ /*me entrega un tipo de dato íntrgro*/
-    printf("Hola mundo \n");
-    return 0;/*siempre que un programa termina en 0 es porque culminó con éxito*/
-}
