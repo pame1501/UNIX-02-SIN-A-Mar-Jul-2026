12e68f408ffd6662d5764b5bc8b63631e1be4f8c
pame1501 <patoapantapi@uide.edu.ec>
2026-05-12 17:09:20 +0000

    umask 077

--- security.zsh
+++ security.zsh
@@ -1,12 +1,8 @@
-umask
-touch archivo1
-mkdir directorio1
-ls -l
-umask 027
-touch archivo2
-mkdir directorio2
-ls -l
-apt update
-apt install acl
-chown -R $(whoami) .
-setfacl -bnR .
+umask: #Displays the current mask (the default permissions).
+touch file1 / mkdir directory1: #Creates a file and a directory with the default settings.
+ls -l: #Lists the files to verify their permissions.
+umask 027:# Sets the new mask (restricts permissions to the group and others).
+apt update / apt install acl: #Updates the package manager and installs the ACL tool, necessary for managing advanced permissions.
+chown -R $(whoami) .: #Changes the owner of all files in the current directory to the user currently logged in.
+setfacl -bnR .: #This is the key command. It recursively removes all ACL rules and restores standard Linux behavior.
+umask 077
