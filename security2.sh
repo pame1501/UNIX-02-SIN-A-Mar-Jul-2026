1d4901d25c4fa079c42890ffc6433b529d2b4d18
pame1501 <patoapantapi@uide.edu.ec>
2026-05-28 12:57:07 +0000

    feat: add pre-newgrp group verification script

--- security2.sh
+++ security2.sh
@@ -4,3 +4,10 @@ id -gn # only the name of principal group
 #Create a file and see wich groups inherits
 touch ~/test_grupo_heredado.txt 
 ls -la ~/test_grupo_heredado.txt #is the user's primary group
+
+#View the current group
+id -gn
+echo "Grupo actual: $(id -gn)"
+#Create a file before newgrp
+touch ~/antes_de_newgrp.txt
+ ls -la ~/antes_de_newgrp.txt
