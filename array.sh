fb37d9a29e63ac1f59199eadf13b9724911a4b97
pame1501 <patoapantapi@uide.edu.ec>
2026-06-04 13:46:45 +0000

    Test array element deletion and execution permissions

--- array.sh
+++ array.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 # Sets an array
 IP_ADDRESSES=(192.168.1.1 192.168.1.2 192.168.1.3)
+IP_ADDRESSES[0]="192.168.1.10"
+unset IP_ADDRESSES[1]
 # Prints all elements in the array
 echo "${IP_ADDRESSES[*]}"
 # Prints only the first element in the array
 echo "${IP_ADDRESSES[0]}"
-chmod u+x array.sh
-IP_ADDRESSES=(192.168.1.1 192.168.1.2 192.168.1.3)
-unset IP_ADDRESSES[1]
