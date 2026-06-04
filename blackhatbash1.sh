192721f1312fe7d7a049195850df508867b5457a
pame1501 <patoapantapi@uide.edu.ec>
2026-06-04 12:36:30 +0000

    docs: log environment variables verification and shell testing

---
+++ blackhatbash1.sh
@@ -0,0 +1,67 @@
+[Pamela-Toapanta] UNIX-02-SIN-A-Mar-Jul-2026  $ bash --version
+GNU bash, versión 5.3.9(1)-release (x86_64-pc-linux-gnu)
+Copyright (C) 2025 Free Software Foundation, Inc.
+Licencia GPLv3+: GPL de GNU versión 3 o posterior <http://gnu.org/licenses/gpl.html>
+
+Esto es software libre, es libre para modificar y redistribuirlo.
+No hay NINGUNA GARANTÍA, a la extensión permitida por la ley.
+[Pamela-Toapanta] UNIX-02-SIN-A-Mar-Jul-2026  $ env
+SHELL=/bin/bash
+COLORTERM=truecolor
+CLOUDENV_ENVIRONMENT_ID=07a6c57a-2882-4df1-a010-5da4175cc94c
+TERM_PROGRAM_VERSION=1.123.0
+GITHUB_USER=pame1501
+CODESPACE_NAME=crispy-space-garbanzo-v6j79j4gqvq6cx7pg
+HOSTNAME=codespaces-7dfae8
+RepositoryName=UNIX-02-SIN-A-Mar-Jul-2026
+LESS_TERMCAP_se=
+LESS_TERMCAP_so=
+CODESPACES=true
+NMAP_PRIVILEGED=
+PWD=/workspaces/UNIX-02-SIN-A-Mar-Jul-2026
+ContainerVersion=13
+VSCODE_GIT_ASKPASS_NODE=/vscode/bin/linux-x64/6a44c352bd24569c417e530095901b649960f9f8/node
+GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN=app.github.dev
+HOME=/root
+GITHUB_API_URL=https://api.github.com
+LANG=es_ES.UTF-8
+
+LS_COLORS=rs=0:di=01;34:ln=01;36:mh=00:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:mi=00:su=37;41:sg=30;43:ca=00:tw=30;42:ow=34;42:st=37;44:ex=01;32:*.7z=01;31:*.ace=01;31:*.alz=01;31:*.apk=01;31:*.arc=01;31:*.arj=01;31:*.bz=01;31:*.bz2=01;31:*.cab=01;31:*.cpio=01;31:*.crate=01;31:*.deb=01;31:*.drpm=01;31:*.dwm=01;31:*.dz=01;31:*.ear=01;31:*.egg=01;31:*.esd=01;31:*.gz=01;31:*.jar=01;31:*.lha=01;31:*.lrz=01;31:*.lz=01;31:*.lz4=01;31:*.lzh=01;31:*.lzma=01;31:*.lzo=01;31:*.pyz=01;31:*.rar=01;31:*.rpm=01;31:*.rz=01;31:*.sar=01;31:*.swm=01;31:*.t7z=01;31:*.tar=01;31:*.taz=01;31:*.tbz=01;31:*.tbz2=01;31:*.tgz=01;31:*.tlz=01;31:*.txz=01;31:*.tz=01;31:*.tzo=01;31:*.tzst=01;31:*.udeb=01;31:*.war=01;31:*.whl=01;31:*.wim=01;31:*.xz=01;31:*.z=01;31:*.zip=01;31:*.zoo=01;31:*.zst=01;31:*.avif=01;35:*.jpg=01;35:*.jpeg=01;35:*.jxl=01;35:*.mjpg=01;35:*.mjpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.svg=01;35:*.svgz=01;35:*.mng=01;35:*.pcx=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.m2v=01;35:*.mkv=01;35:*.webm=01;35:*.webp=01;35:*.ogm=01;35:*.mp4=01;35:*.m4v=01;35:*.mp4v=01;35:*.vob=01;35:*.qt=01;35:*.nuv=01;35:*.wmv=01;35:*.asf=01;35:*.rm=01;35:*.rmvb=01;35:*.flc=01;35:*.avi=01;35:*.fli=01;35:*.flv=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.yuv=01;35:*.cgm=01;35:*.emf=01;35:*.ogv=01;35:*.ogx=01;35:*.aac=00;36:*.au=00;36:*.flac=00;36:*.m4a=00;36:*.mid=00;36:*.midi=00;36:*.mka=00;36:*.mp3=00;36:*.mpc=00;36:*.ogg=00;36:*.ra=00;36:*.wav=00;36:*.oga=00;36:*.opus=00;36:*.spx=00;36:*.xspf=00;36:*~=00;90:*#=00;90:*.bak=00;90:*.crdownload=00;90:*.dpkg-dist=00;90:*.dpkg-new=00;90:*.dpkg-old=00;90:*.dpkg-tmp=00;90:*.old=00;90:*.orig=00;90:*.part=00;90:*.rej=00;90:*.rpmnew=00;90:*.rpmorig=00;90:*.rpmsave=00;90:*.swp=00;90:*.tmp=00;90:*.ucf-dist=00;90:*.ucf-new=00;90:*.ucf-old=00;90::ow=30;44:
+MANROFFOPT=-c
+GIT_ASKPASS=/vscode/bin/linux-x64/6a44c352bd24569c417e530095901b649960f9f8/extensions/git/dist/askpass.sh
+INTERNAL_VSCS_TARGET_URL=https://eastus2.online.visualstudio.com
+SHELL_LOGGED_IN=true
+VSCODE_GIT_ASKPASS_EXTRA_ARGS=
+VSCODE_PYTHON_AUTOACTIVATE_GUARD=1
+GITHUB_CODESPACE_TOKEN=BZK3ZYMPGMGAYTI54J3JFP3KEIJBLANCNFSM4AV6FNNA
+GITHUB_GRAPHQL_URL=https://api.github.com/graphql
+TERM=xterm-256color
+LESS_TERMCAP_mb=
+LESS_TERMCAP_me=
+LESS_TERMCAP_md=
+VSCODE_GIT_IPC_HANDLE=/tmp/vscode-git-4991aab79b.sock
+GITHUB_SERVER_URL=https://github.com
+SHLVL=2
+LESS_TERMCAP_ue=
+LESS_TERMCAP_us=
+GITHUB_REPOSITORY=pame1501/UNIX-02-SIN-A-Mar-Jul-2026
+LC_ALL=es_ES.UTF-8
+VSCODE_GIT_ASKPASS_MAIN=/vscode/bin/linux-x64/6a44c352bd24569c417e530095901b649960f9f8/extensions/git/dist/askpass-main.js
+BROWSER=/vscode/bin/linux-x64/6a44c352bd24569c417e530095901b649960f9f8/bin/helpers/browser.sh
+PATH=/vscode/bin/linux-x64/6a44c352bd24569c417e530095901b649960f9f8/bin/remote-cli:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+CODESPACE_VSCODE_FOLDER=/workspaces/UNIX-02-SIN-A-Mar-Jul-2026
+FEATURE_SPARK_POST_COMMIT_CREATE_ITERATION=true
+DEBIAN_FRONTEND=noninteractive
+OLDPWD=/vscode/bin/linux-x64/6a44c352bd24569c417e530095901b649960f9f8
+TERM_PROGRAM=vscode
+VSCODE_IPC_HOOK_CLI=/tmp/vscode-ipc-8176ac56-1ffb-4182-a769-4094b14b2de3.sock
+_=/usr/bin/env
+[Pamela-Toapanta] UNIX-02-SIN-A-Mar-Jul-2026  $ echo ${SHELL}
+/bin/bash
+[Pamela-Toapanta] UNIX-02-SIN-A-Mar-Jul-2026  $ echo ${RANDOM}
+4341
+[Pamela-Toapanta] UNIX-02-SIN-A-Mar-Jul-2026  $ echo ${UID}
+0
+[Pamela-Toapanta] UNIX-02-SIN-A-Mar-Jul-2026  $ echo ${OSTYPE}
+linux-gnu
+[Pamela-Toapanta] UNIX-02-SIN-A-Mar-Jul-2026  $ 
