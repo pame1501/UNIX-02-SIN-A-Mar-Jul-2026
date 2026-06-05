cc56fa8632d8d4a5d319a3113edc01d2a1ecfb00
pame1501 <patoapantapi@uide.edu.ec>
2026-06-05 16:49:54 +0000

    feat: add script demonstrating background sleep and file management

--- array.sh
+++ array.sh
@@ -2,7 +2,7 @@
 # Sets an array
 IP_ADDRESSES=(192.168.1.1 192.168.1.2 192.168.1.3)
 IP_ADDRESSES[0]="192.168.1.10"
-unset IP_ADDRESSES[1]
+unset "IP_ADDRESSES[1]"
 # Prints all elements in the array
 echo "${IP_ADDRESSES[*]}"
 # Prints only the first element in the array
