74fc2a61eeb938f11c1ff253415d778a8d20ca73
pame1501 <patoapantapi@uide.edu.ec>
2026-06-05 17:25:39 +0000

    feat: add script for logical operators, I/O redirections and heredocs

--- blackhatbash5.sh
+++ blackhatbash5.sh
@@ -6,16 +6,16 @@ ls; ps; whoami
 lzl || echo "El comando lzl falló"
 
 echo "Hello World!" > output.txt
- cat output.txt
- echo "Chao Mundo!" > output.txt
- cat output.txt
- echo "Chao Chao" >> output.txt
- cat output.txt
+cat output.txt
+echo "Chao Mundo!" > output.txt
+cat output.txt
+echo "Chao Chao" >> output.txt
+cat output.txt
 
- ls -l / &> stdout_and_stderr.txt
- cat stdout_and_stderr.txt
+ls -l / &> stdout_and_stderr.txt
+cat stdout_and_stderr.txt
 
- ls -l / &>> stdout_and_stderr.txt
+ls -l / &>> stdout_and_stderr.txt
 cat stdout_and_stderr.txt
 
 ls -l / 1> stdout.txt 2> stderr.txt
@@ -24,3 +24,13 @@ cat stderr.txt
 
 lzl 2> error.txt
 cat error.txt
+
+cat < output.txt
+
+cat << EOF
+Black Hat Bash
+by No Starch Press
+EOF
+
+ls -l
+ls -l / | grep "bin"
