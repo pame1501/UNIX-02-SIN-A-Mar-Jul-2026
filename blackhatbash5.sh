78c70160bd0b6ff430cf934173992b3ff35be890
pame1501 <patoapantapi@uide.edu.ec>
2026-06-05 17:16:05 +0000

    feat: add script examples for bash redirections and logical operators

--- blackhatbash5.sh
+++ blackhatbash5.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
-top
-top &
+#top
+#top &
+touch test && touch test123
+ls; ps; whoami
+lzl || echo "El comando lzl falló"
+
+echo "Hello World!" > output.txt
+ cat output.txt
+ echo "Chao Mundo!" > output.txt
+ cat output.txt
+ echo "Chao Chao" >> output.txt
+ cat output.txt
+
+ ls -l / &> stdout_and_stderr.txt
+ cat stdout_and_stderr.txt
+
+ ls -l / &>> stdout_and_stderr.txt
+cat stdout_and_stderr.txt
+
+ls -l / 1> stdout.txt 2> stderr.txt
+cat stdout.txt
+cat stderr.txt
+
+lzl 2> error.txt
+cat error.txt
