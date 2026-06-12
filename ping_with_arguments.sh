888d308aba64db0f8fc1393417fdae24a2965123
pame1501 <patoapantapi@uide.edu.ec>
2026-06-12 03:57:10 +0000

    feat: add interactive user input for first and last name

--- ping_with_arguments.sh
+++ ping_with_arguments.sh
@@ -12,3 +12,10 @@ echo "The total number of arguments is: $#"
 for args in "$@"; do
  echo "${args}"
 done
+
+echo "What is your first name?"
+read -r firstname
+echo "What is your last name?"
+read -r lastname
+echo "Your first name is ${firstname} and your last name is
+${lastname}"
