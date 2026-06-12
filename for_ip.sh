6c6c3876322d2be729a86528025262da40f1bf63
pame1501 <patoapantapi@uide.edu.ec>
2026-06-12 17:10:09 +0000

    feat: add ping utility to ip processing loop

--- for_ip.sh
+++ for_ip.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 for ip_address in "$@"; do
  echo "Taking some action on IP address ${ip_address}"
+ ping -c 3 "${ip_address}"
 done
+
+#functional ip
+#github.com = 140.82.112.4
+#127.0.0.1
+#localhost
