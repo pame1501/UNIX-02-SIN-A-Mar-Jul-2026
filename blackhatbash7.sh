217f38143677dc58c1da67dfb0ba5161d4e8a194
pame1501 <patoapantapi@uide.edu.ec>
2026-06-18 13:10:49 +0000

    Add log analysis examples using head, grep, and awk piping for data extraction

--- blackhatbash7.sh
+++ blackhatbash7.sh
@@ -13,3 +13,18 @@ awk '{print $1}' log.txt
 awk '{print $1,$2,$3}' log.txt
 awk '{print $2}' log.txt
 awk '{print $3}' log.txt
+
+awk '{print $1,$NF}' log.txt
+
+touch example_csv.txt
+echo "Hola1,chao1">> example_csv.txt
+echo "Hola2,chao2">> example_csv.txt
+echo "Hola3,chao3">> example_csv.txt
+cat example_csv.txt
+awk -F',' '{print $1}' example_csv.txt
+
+head log.txt
+awk 'NR < 10' log.txt
+grep "42.236.10.117" log.txt
+awk '{print $7}' log.txt
+grep "42.236.10.117" log.txt | awk '{print $7}'
